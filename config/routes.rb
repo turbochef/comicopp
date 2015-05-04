8b79c979184fc2c3f6f572fc179c9a495ee3f73e
Daniel R. Soetanto <turbochef.ds@gmail.com>
2015-05-04 00:55:59 -0500

    added users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
